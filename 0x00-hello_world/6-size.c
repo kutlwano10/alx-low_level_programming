00e5e68f20ca86b3a1ebc14d5df87f4425874d79
kutlwano10 <kutlwano.ramotebele@gmail.com>
2022-09-09 04:53:09 -0700

    task 6 done with space corretion

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
 /**
-* main - print a string in to printf function
-* 
-* Description: we use the main function
-* this program prints " the size of various types on the computer it is compiled and run on
-* return: 0
+* main - print the string in printf function
+* Description: using main function
+* this program prints "the size of various types on the computer
+* Return: 0
 */
 int main(void)
 {
 	char c;
 	int i;
-	long l;
-	long long ll;
+	long li;
+	long long lli;
 	float f;
 
-	printf("Size of char: %ld byte(s)\n", sizeof(c));
-	printf("Size of int: %ld byte(s)\n", sizeof(i));
-	printf("Size of long int: %ld byte(s)\n", sizeof(l));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(ll));
-	printf("Size of float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
-	
