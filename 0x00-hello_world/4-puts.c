5dbec7065de6327f3dc0669bdec85c6013f8d7c9
kutlwano10 <kutlwano.ramotebele@gmail.com>
2022-09-08 17:03:38 -0700

    Task 4 done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * Using puts to print a string
+ *
+ * Description : we are printing "Programming is like a multilingual puzzle
+ * Return : 0
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
