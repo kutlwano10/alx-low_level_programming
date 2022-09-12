52e3ca5d91417be679a68f37cf8c9879f54b971e
kutlwano10 <kutlwano.ramotebele@gmail.com>
2022-09-12 09:20:28 -0700

    task 7 done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - using the main function
+*
+* Description: using the putchar function
+* this program prints "the lowercase alphabets in lowercase
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
