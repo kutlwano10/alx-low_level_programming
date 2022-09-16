083b70288823679c47b42c8474ce56ea1365ed4c
kutlwano10 <kutlwano.ramotebele@gmail.com>
2022-09-16 04:59:37 -0700

    done task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - cause an infinite loop
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	* while (1 < 10)
+	* {
+	* putchar(i);
+	* }
+	*/
+}
