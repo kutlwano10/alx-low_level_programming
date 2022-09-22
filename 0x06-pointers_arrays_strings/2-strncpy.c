760b79d17315332c81db04d0f290a740104cfb13
kutlwano10 <kutlwano.ramotebele@gmail.com>
2022-09-22 05:40:36 -0700

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp - for copying purpose
+* @s1: param1
+* @s2: param2
+* Return: integer
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strcmp(s1, s2);
+	return (dest);
+}
